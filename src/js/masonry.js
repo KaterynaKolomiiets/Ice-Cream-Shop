2b6eee7a0637ab7696004bd039255ae22b334d19
KaterynaKolomiiets <kateryna.kolomiiets.official@gmail.com>
2021-09-01 21:59:45 +0300

    л

--- src/js/masonry.js
+++ src/js/masonry.js
@@ -1,4 +1,12 @@
-$('.gallery-masonry').masonry({
-    itemSelector: '.element-item',
-    columnWidth: 300
+$('.grid').masonry({
+    itemSelector: '.grid__item',
+  
 });
+
+// window.onload = () => {
+//     const grid = document.querySelector('.grid');
+//     const masonry = new Masonry (grid, {
+//         itemSelector: 'grid__item',
+//         gutter: 10,
+//     });
+// }
