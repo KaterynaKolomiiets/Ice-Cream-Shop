9a5d96f9e40179f5b48e1925cb320d4cb829b71f
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2021-09-02 21:36:07 +0300

    finish-changes

--- src/js/modal-footer-window.js
+++ src/js/modal-footer-window.js
@@ -12,3 +12,18 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalHeadBtn: document.querySelector('[footer-secand-data-modal-open]'),
+    closeModalHeadBtn: document.querySelector('[footer-secand-data-modal-close]'),
+    modal: document.querySelector('[footer-secand-data-modal]'),
+  };
+
+  refs.openModalHeadBtn.addEventListener('click', toggleModal);
+  refs.closeModalHeadBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
