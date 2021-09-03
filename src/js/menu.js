bdc857a47139ff87fe3d4ae98bf48c5a6fe77196
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2021-09-03 16:14:21 +0300

    mobile-menu-close

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,13 +3,20 @@
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
+    menuList: document.querySelector(".menu__list"),
+
   };
 
   refs.openMenuBtn.addEventListener("click", toggleModal);
   refs.closeMenuBtn.addEventListener("click", toggleModal);
+  refs.menuList.addEventListener("click", removeModal);
 
   function toggleModal() {
     refs.menu.classList.toggle("is-open");
+  }
+
+    function removeModal() {
+    refs.menu.classList.remove("is-open");
   }
   
 })();
